3278fc4c97b9e9978ded00708d8f9ec6390c3aa0
modshirnk <mayoibi@modshrink.com>
2015-09-09 21:33:00 +0900

    remove: 不要なSQL

--- sidebar.php
+++ sidebar.php
@@ -7,16 +7,3 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 <div id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary -->
-
-
-<ul>
-<?php
-$sql = "SELECT * FROM $wpdb->posts
-	WHERE post_type = 'post' AND post_status = 'publish'
-	ORDER BY post_date DESC LIMIT 3";
-$rows = $wpdb->get_results($sql);
-foreach ($rows as $row) {
-	echo "<li>$row->post_title</li>";
-}
-?>
-</ul>
