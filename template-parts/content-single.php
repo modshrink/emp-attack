23fb2710ad9fd2983256886348c22ff8997a3e4d
modshirnk <mayoibi@modshrink.com>
2015-09-18 21:12:20 +0900

    modify: スタイル改修中

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -1,11 +1,11 @@
 		<article id="post-<?php the_ID(); ?>" <?php post_class( array( 'article' ) ); ?>>
-			<header>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
+			<header class="article__header">
+				<h1 class="article__header__title"><?php the_title(); ?></h1>
 			</header>
-			<div class="entry-content">
+			<div class="article__conent">
 				<?php the_content(); ?>
 			</div>
-			<footer>
+			<footer class="article__footer">
 				<div class="bookmark"><a href="<?php the_permalink(); ?>">Permalink</a></div>
 				<div class="published"><?php echo get_the_date( 'Y-m-d' ); ?></div>
 				<div class="tags"><?php the_tags(); ?></div>
