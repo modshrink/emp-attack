d1fdc55b079b3b9e3a8268fa5d3f02fc94d3d26d
mayoibi <mayoibi@modshrink.com>
2015-09-22 04:42:55 +0900

    modify: レイアウト変更

--- front-page.php
+++ front-page.php
@@ -2,26 +2,7 @@
 
 	<main role="main">
 		<?php while ( have_posts() ) : the_post(); ?>
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<header>
-					<div class="published"><?php echo get_the_date( 'Y-m-d' );?></div>
-					<h1 class="entry-title"><?php the_title(); ?></h1>
-				</header>
-				<div class="entry-content">
-					<?php if ( has_post_thumbnail() ) : ?>
-						<figure class="thumbnail">
-						<?php the_post_thumbnail( 'thumbnail' ); ?>
-						</figure>
-					<?php endif; ?>
-					<?php the_excerpt(); ?>
-				</div>
-				<footer>
-					<ul>
-						<li class="bookmark"><a href="<?php the_permalink(); ?>">Permalink</a></li>
-						<li class="published">Last modified: <?php the_modified_time( 'Y-m-d' );?></li>
-					</ul>
-				</footer>
-			</article>
+			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 		<?php endwhile; ?>
 		<nav class="paginate">
 		<?php echo paginate_links(); ?>
